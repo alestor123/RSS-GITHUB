395f30927d62a0cece3f13cfdbe287b7710d1a69
alestor123 <alestoraldous@gmail.com>
2020-12-18 12:26:58 +0530

    added logger

--- App.js
+++ App.js
@@ -4,5 +4,9 @@ require('dotenv').config()
 var express = require('express'),
 app = express(),
 argv = process.argv[2],
+chalk = require('chalk'),
 port = process.env.PORT || argv || 3000;
-logger = require('logfs');
+app.listen(port, () => console.log(chalk.bgYellow.red(logger(`Server running at ${port}`))))
+function logger(message){
+return `(LOG):${Date()}:${message}`
+}
